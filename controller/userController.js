419d4a4164e55a572a2fa8c8263311897464350c
Gabriel Cardoso <gr78787@gmail.com>
2022-11-17 13:30:42 -0300

    add method get and login users

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,26 @@
-const User = require("../models/USER");
+const User = require('../models/USER');
+const bcrypt = require('bcryptjs');
 
 const userController = {
 
+
+  users: async(req, res)=>{
+    const getUsers = await User.find({User})
+    return res.send(getUsers)
+  },
+
   register: async (req, res) => {
+
+    const selectedUser =  await User.findOne({email: req.body.email})
+    if(selectedUser){
+        return res.status(400).send('Esse email já se encontra cadastrado em nosso sistema!')
+    }
+
+
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: bcrypt.hashSync(req.body.password)
     });
 
     try {
@@ -19,8 +33,19 @@ const userController = {
   },
 
 
-  login: (req, res) => {
-    res.send("Login");
+  login: async (req, res) => {
+
+    const selectedUser =  await User.findOne({email: req.body.email})
+    if(!selectedUser){
+        return res.status(400).send('Email ou senha incorreta!')
+    }
+
+    const passwordAndUserMatch =  bcrypt.compareSync(req.body.password, selectedUser.password)
+      if(!passwordAndUserMatch){
+        return res.status(400).send('Email ou senha incorreta!')
+      }
+
+      res.status(200).send('Usuário logado com sucesso!')
   },
 };
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,6 @@ const userController = require("../controller/userController");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
+router.get("/users", userController.users);
 
 module.exports = router;
